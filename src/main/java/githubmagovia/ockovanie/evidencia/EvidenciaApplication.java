387ba50e19788857cc9ebe768641b31d9cef1b42
Michal Mojžiš <missko111@gmail.com>
2022-03-26 11:35:52 +0100

    Spring Initializr + Structure

---
+++ src/main/java/githubmagovia/ockovanie/evidencia/EvidenciaApplication.java
@@ -0,0 +1,13 @@
+package githubmagovia.ockovanie.evidencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvidenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvidenciaApplication.class, args);
+	}
+
+}
