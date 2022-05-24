64a2dc1a0f99c2e2ba478c05f94800b9e5cfae21
Michal Mojžiš <missko111@gmail.com>
2022-05-24 18:09:00 +0200

    test

---
+++ src/test/java/githubmagovia/ockovanie/evidencia/models/PersonEntityTest.java
@@ -0,0 +1,41 @@
+package githubmagovia.ockovanie.evidencia.models;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import githubmagovia.ockovanie.evidencia.person.PersonRepository;
+import githubmagovia.ockovanie.evidencia.person.models.Gender;
+import githubmagovia.ockovanie.evidencia.person.models.PersonEntity;
+import githubmagovia.ockovanie.evidencia.vaccination.models.VaccinationStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+public class PersonEntityTest {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    public void shouldSavePersonEntity() {
+        PersonEntity person = new PersonEntity();
+        person.setFirstName("Mark");
+        person.setLastName("Mark");
+        person.setSex(Gender.MALE);
+        person.setDateOfBirth(LocalDate.now());
+        person.setStatus(VaccinationStatus.NONE);
+        personRepository.save(person);
+        assertThat(person.getId()).isNotNull();
+    }
+
+    @Test
+    public void savingUncompletePersonEntityShouldThrowException() {
+        PersonEntity person = new PersonEntity();
+        person.setFirstName("Mark");
+        assertThatThrownBy(() -> personRepository.save(person));
+
+    }
+}
