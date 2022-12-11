129d5a79c2d19ea929613a68a0672e944a407180
Michal Mojžiš <missko111@gmail.com>
2022-12-11 14:35:31 +0100

    rabbitmq publishing

--- src/main/java/githubmagovia/ockovanie/evidencia/rabbitmq/EmailListener.java
+++ src/main/java/githubmagovia/ockovanie/evidencia/rabbitmq/EmailListener.java
@@ -1,7 +1,9 @@
 package githubmagovia.ockovanie.evidencia.rabbitmq;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EmailListener {
 
     @RabbitListener(queues = "emailQueue")
