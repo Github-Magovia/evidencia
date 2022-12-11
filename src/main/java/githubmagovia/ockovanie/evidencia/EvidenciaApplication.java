82f3c413d97c66e92f0055310651712eb344e461
Michal Mojžiš <missko111@gmail.com>
2022-12-11 14:25:34 +0100

    rabbitmq publishing

--- src/main/java/githubmagovia/ockovanie/evidencia/EvidenciaApplication.java
+++ src/main/java/githubmagovia/ockovanie/evidencia/EvidenciaApplication.java
@@ -1,12 +1,8 @@
 package githubmagovia.ockovanie.evidencia;
 
-import githubmagovia.ockovanie.evidencia.rabbitmq.MessageSubscriber;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
 
 @SpringBootApplication
 public class EvidenciaApplication {
